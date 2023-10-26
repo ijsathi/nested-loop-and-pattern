b1e1b9edf0e0b6240df36b85c1b5e4259f36f0a3
Israt Jahan Sathi <ijsathi2001rs@gmail.com>
2023-10-26 15:38:07 +0600

    sorting:

---
+++ sorting.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    // ascending
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] > a[j]) // descending e sudhu '<' eta change kore dite hobe
+            {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
